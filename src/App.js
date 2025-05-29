86eb1233859a784b9c6af6fd0c1335903d9c7fef
Arvin Bonggal <arvinbonggal@gmail.com>
2025-05-29 10:23:23 +0800

    Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
         onDeleteItems={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -141,11 +141,29 @@ function Item({ item, onDeleteItems, onToggleItem }) {
 }
 
 // Stats component to display the statistics of the packing list
-function Stats() {
+function Stats({ items }) {
+  // Early return if there are no items
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list!</em>
+      </p>
+    );
+
+  // Derive statistics from the items array
+  // Calculate the number of items, packed items, and percentage packed
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentagePacked = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
       <em>
-        💼 You have x items on your list, and you have already packed x (x%)
+        {/* Display different messages based on the percentage packed */}
+        {percentagePacked === 100
+          ? "You got everything! Ready to go!"
+          : `
+        💼 You have ${numItems} items on your list, and you have already packed ${numPacked} ${percentagePacked}%`}
       </em>
     </footer>
   );
